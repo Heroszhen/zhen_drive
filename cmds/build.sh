9e5ebf5d0aecfd9932cb4b2928ac10fe2ed4794e
zhen <zhen.yang@hotmail.fr>
2025-11-02 12:05:51 +0100

    maj

--- cmds/build.sh
+++ cmds/build.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 #build for dev or production
 
+date
 mv .env.local dist.env.local
 mv .env.dist.prod.local .env.prod.local
+sleep
 
 echo `pwd`
 npm run build
 
 mv dist.env.local .env.local
 mv .env.prod.local .env.dist.prod.local
+date
