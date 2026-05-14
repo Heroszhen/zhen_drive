197beebfdceff6d88a619bda45b30f9ba5b66a65
zhen <zhen.yang@hotmail.fr>
2026-05-14 13:54:54 +0200

    maj

--- assets/react/components/menuactions/MenuActions.jsx
+++ assets/react/components/menuactions/MenuActions.jsx
@@ -23,7 +23,7 @@ const MenuActions = () => {
           <i className="bi bi-book"></i>
           <span>Ouvrir</span>
         </div>
-        {driveIndex && !drive?.[driveIndex]['fullName'].endsWith('/') && (
+        {driveIndex !== null && !drive?.[driveIndex]['fullName'].endsWith('/') && (
           <>
             <div
               className="action"
